104a576e88bd27ad2dd739dd94e33d8d5654b603
Abdullah Nasir <malikabdullahnasir5@gmail.com>
2025-05-24 23:09:16 +0500

    Delete app directory

--- app/home/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center bg-black">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-primary border-t-transparent" />
-    </div>
-  )
-}
